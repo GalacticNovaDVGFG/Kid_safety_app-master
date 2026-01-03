4505016cd53fa438c2ad2747aabd5424adfa9b65
GalacticNovaDVGFG <musinikushal@gmail.com>
2026-01-02 18:44:13 -0800

    fix(guardian): use stopMedia() in cleanup (stopCamera was undefined)

--- src/components/guardian-keychain.tsx
+++ src/components/guardian-keychain.tsx
@@ -264,7 +264,7 @@ export default function GuardianKeychain() {
     return () => {
       stopAnalysis();
       stopLocationTracking();
-      stopCamera();
+      stopMedia();
     };
   }, []);
   
